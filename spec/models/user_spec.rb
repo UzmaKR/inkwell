02a5f77430ed0976e36ef6d7ef717fb85ef8e756
Uzma Rahman <uzma.rahman@gmail.com>
2013-08-17 16:26:30 -0700

    Tested password_required? method in User.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,33 @@ describe User do
   let!(:user) {create(:user)}
 
   it { should validate_presence_of(:email)}
-  # it { should validate_presence_of(:encrypted_password)}
-  # it { should validate_presence_of(:first_name)}
-  # it { should validate_presence_of(:last_name)}
+  it { should validate_presence_of(:encrypted_password)}
+  it { should validate_uniqueness_of(:email)}
+  it { should validate_confirmation_of(:password)}
 
   it { should have_many(:friends) }
   it { should have_many(:occasions) }
   it { should have_many(:orders) }
+  it { should have_many(:authentications)}
 
   it "should have no friends on creation" do
     expect(user.friends.count).to be 0
   end
+
+  describe "#password_required?" do
+
+    it "password required if password is blank" do
+      @user = build(:user, password: " " )
+      expect(@user.password_required?).to eq(true)
+    end
+
+    it "password not required if password not blank" do
+      expect(user.password_required?).to eq(false)
+    end
+  end
+
+  #describe "#name" do
+
+  #end
 
 end
